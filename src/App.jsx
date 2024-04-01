a121535d9e8b4664d69e9a3dc6065b7267bb0e61
mohi14 <mohiuddinkhan1409@gmail.com>
2024-04-01 15:22:11 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,64 @@
 import './App.css'
 import { useCountdown } from './hooks/useCountdown';
 import audio from "./assets/audio1.mp3"
+import audio1 from "./assets/audio.mp3"
 import { useEffect, useRef, useState } from 'react';
+import Confetti from 'react-confetti'
 
 function App() {
 
-  const targetDate = new Date('April 10, 2024 23:59:00 GMT+0600').toISOString();
+  const targetDate = new Date('April 7, 2024 14:00:00 GMT +0600').toISOString();
   const [days, hours, minutes, seconds] = useCountdown(targetDate);
-  const [audioLoaded, setAudioLoaded] = useState(false);
+
+
 
   const audioRef = useRef(null);
-
-  const banner = useRef(null)
-
-  const handlePlay = () => {
-
-    setTimeout(() => {
-      audioRef.current.play();
-    }, 2000);
+  const audioRef1 = useRef(null);
 
 
-  };
-
-  useEffect(() => {
-    // Check if audio is loaded
-    if (audioRef.current.readyState >= 3) {
-      setAudioLoaded(true);
-    } else {
-      // If not loaded, listen for 'canplaythrough' event to set loaded state
-      audioRef.current.addEventListener('canplaythrough', () => {
-        setAudioLoaded(true);
-      });
-    }
-  }, []);
-
-  useEffect(() => {
-    // Play audio if loaded and countdown reaches zero
-    if ((days + hours + minutes + seconds <= 0) && audioLoaded) {
-      audioRef.current.play();
-    }
-  }, [days, hours, minutes, seconds, audioLoaded]);
 
 
 
   if ((days + hours + minutes + seconds <= 0)) {
-    return <div>okay</div>
+    return <section className='banner' onClick={() => audioRef1.current.play()}>
+      <Confetti />
+      <audio ref={audioRef1} autoPlay loop preload='auto' style={{ display: "none" }} >
+        <source src={audio1} type="audio/mp3" />
+      </audio>
+      <div>
+        <h1>Eid Vacation</h1>
+
+        <div className='timerContainer'>
+          <div>
+            <p className='timerNumber'>0</p>
+            <p className='timerLabel'>DAYS</p>
+          </div>
+          <div>
+            <p className='timerNumber'>0</p>
+            <p className='timerLabel'>HOURS</p>
+          </div>
+          <div>
+            <p className='timerNumber'>0</p>
+            <p className='timerLabel'>MINUTES</p>
+          </div>
+          <div>
+            <p className='timerNumber'>0</p>
+            <p className='timerLabel'>SECONDS</p>
+          </div>
+        </div>
+      </div>
+    </section>
   }
 
 
 
   return (
-    <section className='banner' ref={banner}>
-      <audio ref={audioRef} autoPlay loop preload='on' style={{ display: "none" }} >
+    <section className='banner' onClick={() => audioRef.current.play()}>
+      <audio ref={audioRef} autoPlay loop preload='auto' style={{ display: "none" }} >
         <source src={audio} type="audio/mp3" />
       </audio>
       <div>
-        <h1>Eid Mubarak</h1>
+        <h1>Eid Vacation</h1>
 
         <div className='timerContainer'>
           <div>
